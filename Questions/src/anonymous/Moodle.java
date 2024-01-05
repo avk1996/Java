5b4e382b9818052db69dec614983a8c245737e7d
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-01-05 22:32:44 +0530

    some practice questions

---
+++ Questions/src/anonymous/Moodle.java
@@ -0,0 +1,16 @@
+package anonymous;
+
+interface Remote {
+	public void test();
+}
+
+public class Moodle {
+	public static void main(String argv[]) {
+		Moodle m = new Moodle();
+		System.out.println("Hello world");
+	}
+
+	public void go() {
+//here 
+	}
+}
