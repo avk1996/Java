736db05fe0a8fbc233d3fb3bbc55c8ec843ac063
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-01-08 22:39:16 +0530

    ternary operations

--- Questions/src/basics/TernaryExample1.java
+++ Questions/src/basics/TernaryExample1.java
@@ -5,5 +5,10 @@ public class TernaryExample1 {
 		int a = 5, b = 2, c = 3;
 		int larg = (c > (a > b ? a : b) ? c : (a > b ? a : b));
 		System.out.println("Largest of three is : "+ larg);
+		
+		float m;
+		m = 1.4f;
+		System.out.println("Type of : "+typeof(m));
+		m = (float) (m*23.44);
 	}
 }
