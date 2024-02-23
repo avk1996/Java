88e3632be6ee8a3d7b19de560c1e12a08f1f5c32
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-02-23 16:00:22 +0530

    started solving longest common prefix in strings

--- LeetCodeSolvedProblems/src/LongestPrefix.java
+++ LeetCodeSolvedProblems/src/LongestPrefix.java
@@ -2,8 +2,22 @@
 public class LongestPrefix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		String[] strs = {"flower","flow","flight"};
+		String same = longestCommonPrefix(strs);
+		System.out.println("longest: "+strs);
+		
+	}
+	public static String longestCommonPrefix(String[] strs) {
+		String str1 = strs[0];
+		String same = "";
+		for(int i=1;i<strs.length;i++) {
+			for(int j=0;j<strs[i].length();j++) {
+				if(str1.charAt(j) == strs[i].charAt(j)) {
+					same = same + str1.charAt(j);
+				}
+			}
+		}
+		return same;
 	}
 
 }
