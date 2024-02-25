089060bcabc2654cc4ca34f44cd81e0272ac7a6d
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-02-25 12:53:15 +0530

    reverse words using String functions

--- StringProblems/src/ReverseWords.java
+++ StringProblems/src/ReverseWords.java
@@ -2,16 +2,34 @@ import java.util.Scanner;
 
 public class ReverseWords {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String string = sc.nextLine();
-		String[] stringArray = new String[100];
-		char[] newString = string.toCharArray();
-		int count = 0;
-		for (int i = 0; i < string.length(); i++) {
-			if(newString[i]!=' ') {
-				stringArray[i] = newString[count]; 
-			}
-		}
-		sc.close();
+		String letters[] = new String[10];
+		String s = "hell world";
+		int j = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) ==  ' '){
+                System.out.println(j+". "+s.charAt(i));
+                ++j;
+            }
+            else{
+                System.out.println(j+" - "+s.charAt(i));
+                letters[j] = letters[j] + s.charAt(i);
+            }
+        }
+        String s1 = "";
+        for(int i=0;i<letters.length;i++){
+            System.out.println(letters[i]+" ");
+        }
 	}
+	public String reverseWords(String s) {
+        String letters[] = s.split("\\s+");
+        String returnStr = "";
+        for(int i=letters.length-1;i>=0;i--){
+            System.out.println(letters[i]+" ");
+            returnStr = returnStr + letters[i] + " ";
+        }        
+        returnStr = returnStr.trim();
+        
+        return returnStr;
+    }
 }
