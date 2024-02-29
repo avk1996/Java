46e0d1507ece4a1f78b4aabbd41f84713055b497
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-02-29 13:47:47 +0530

    count the pairs sum method 2

--- ArrayProblems/src/CountPairSums.java
+++ ArrayProblems/src/CountPairSums.java
@@ -5,9 +5,10 @@ public class CountPairSums {
 		int[] arr = {1,1,1,1};
 		int k = 2;
 		System.out.println("count pairs: "+countPairSumsDistinct(arr, k));
+		System.out.println("count pairs: "+countPairSums(arr, k));
 	}
 
-	//count the pair of distinct pairs of sums
+	// count the pair of distinct pairs of sums
 	public static int countPairSumsDistinct(int[] arr,int k) {
 		int count = 0;
 		if(arr.length == 0){
@@ -17,6 +18,25 @@ public class CountPairSums {
 			int sum = 0;
 			for(int j=i;j<arr.length;j++) {
 				sum = sum  + arr[j];				
+				if(sum == k) {
+					count++;
+					break;
+				}
+			}
+		}
+		return count;
+	}
+	
+	// count the pairs of distinct sums method 2
+	public static int countPairSums(int[] arr,int k) {
+		int count = 0;
+		if(arr.length == 0){
+			return count;
+		}
+		for(int i=0;i<arr.length;i++) {
+			int sum = 0;
+			for(int j=i+1;j<arr.length;j++) {
+				sum = arr[i] + arr[j];				
 				if(sum == k) {
 					count++;
 					break;
