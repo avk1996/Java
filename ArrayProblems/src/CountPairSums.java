4f041dd67bf052ee54123d1ff8219a1d31f39afc
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-02-29 13:51:37 +0530

    no. of pairs which are not distinct 1

--- ArrayProblems/src/CountPairSums.java
+++ ArrayProblems/src/CountPairSums.java
@@ -2,23 +2,23 @@
 public class CountPairSums {
 
 	public static void main(String[] args) {
-		int[] arr = {1,1,1,1};
-		int k = 2;
-		System.out.println("count pairs: "+countPairSumsDistinct(arr, k));
-		System.out.println("count pairs: "+countPairSums(arr, k));
+		int[] arr = { 1, 5, 7, -1, 5 };
+		int k = 6;
+		System.out.println("count pairs: " + countPairSumsDistinct(arr, k));
+		System.out.println("count pairs: " + countPairSums(arr, k));
 	}
 
 	// count the pair of distinct pairs of sums
-	public static int countPairSumsDistinct(int[] arr,int k) {
+	public static int countPairSumsDistinct(int[] arr, int k) {
 		int count = 0;
-		if(arr.length == 0){
+		if (arr.length == 0) {
 			return count;
 		}
-		for(int i=0;i<arr.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
 			int sum = 0;
-			for(int j=i;j<arr.length;j++) {
-				sum = sum  + arr[j];				
-				if(sum == k) {
+			for (int j = i; j < arr.length; j++) {
+				sum = sum + arr[j];
+				if (sum == k) {
 					count++;
 					break;
 				}
@@ -26,36 +26,35 @@ public class CountPairSums {
 		}
 		return count;
 	}
-	
+
 	// count the pairs of non-distinct sums method 2
-	public static int countPairSums(int[] arr,int k) {
+	public static int countPairSums(int[] arr, int k) {
 		int count = 0;
-		if(arr.length == 0){
+		if (arr.length == 0) {
 			return count;
 		}
-		for(int i=0;i<arr.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
 			int sum = 0;
-			for(int j=i+1;j<arr.length;j++) {
-				sum = arr[i] + arr[j];				
-				if(sum == k) {
+			for (int j = i + 1; j < arr.length; j++) {
+				sum = arr[i] + arr[j];
+				if (sum == k) {
 					count++;
 				}
 			}
 		}
 		return count;
 	}
-	
-	// count the pairs of non-distinct sums method 
-	public static int countPairSumsNonDistinct(int[] arr,int k) {
+
+	// count the pairs of non-distinct sums method
+	public static int countPairSumsNonDistinct(int[] arr, int k) {
 		int count = 0;
-		if(arr.length == 0){
+		if (arr.length == 0) {
 			return count;
 		}
-		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[i] + arr[j]== k) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == k) {
 					count++;
-					break;
 				}
 			}
 		}
