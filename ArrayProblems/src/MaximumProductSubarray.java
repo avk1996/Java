e7369a3c231552b010030158875db76686e33248
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-03-25 18:26:37 +0530

    finding the maximum product subarray using Kadane's algo

---
+++ ArrayProblems/src/MaximumProductSubarray.java
@@ -0,0 +1,29 @@
+
+public class MaximumProductSubarray {
+
+	public static void main(String[] args) {
+		int[] nums = {-3,-1,-1};
+		System.out.println("Max product: "+maxProduct(nums));
+	}
+
+	private static int maxProduct(int[] nums) {
+		int leftProduct = 1;
+		int rightProduct = 1;
+		int ans = nums[0];
+		int n = nums.length;
+		
+		for (int i = 0; i < n; i++) {
+			leftProduct = leftProduct == 0 ? 1:leftProduct;
+			rightProduct = rightProduct == 0 ? 1:rightProduct;
+			
+			leftProduct *= nums[i];
+			
+			rightProduct *= nums[n-i-1];
+			
+			ans = Math.max(ans,Math.max(leftProduct, rightProduct));
+		}
+		
+		return ans;
+	}
+
+}
