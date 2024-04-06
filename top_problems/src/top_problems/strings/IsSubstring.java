8b077a783db394b22015956c077dcd9d4ce89670
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-06 14:36:31 +0530

    is substring check

---
+++ top_problems/src/top_problems/strings/IsSubstring.java
@@ -0,0 +1,40 @@
+package top_problems.strings;
+
+public class IsSubstring {
+
+	public static void main(String[] args) {
+		String s = "aec";
+		String t = "abcde";
+		boolean isSubsequence = isSubsequence(s, t);
+		System.out.println("is subsequence: " + isSubsequence);
+	}
+
+	private static boolean isSubsequence(String s, String t) {
+
+		if (s.length() == 0 && t.length() == 0)
+            return true;
+        else if (t.length() == 0)
+            return false;
+        else if (s.length() == 0)
+            return true;
+        else if(s.length() > t.length())
+            return false;
+
+        int i = 0, j = 0;
+        int max = Math.max(s.length(), t.length());
+        int min = Math.min(s.length(), t.length());
+        for (int j2 = 0; j2 < max; j2++) {
+            if (i < min && s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        if (i == min)
+            return true;
+        else
+            return false;
+	}
+
+}
