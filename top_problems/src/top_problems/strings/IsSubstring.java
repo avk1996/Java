e33d982faaa2a3da547020d0213be87cd8286887
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-06 14:43:50 +0530

    is substring improved space

--- top_problems/src/top_problems/strings/IsSubstring.java
+++ top_problems/src/top_problems/strings/IsSubstring.java
@@ -11,30 +11,21 @@ public class IsSubstring {
 
 	private static boolean isSubsequence(String s, String t) {
 
-		if (s.length() == 0 && t.length() == 0)
-            return true;
-        else if (t.length() == 0)
-            return false;
-        else if (s.length() == 0)
-            return true;
-        else if(s.length() > t.length())
-            return false;
+		if ((s.length() == 0 && t.length() == 0) || s.length() == 0)
+			return true;
+		else if (s.length() > t.length() || t.length() == 0)
+			return false;
 
-        int i = 0, j = 0;
-        int max = Math.max(s.length(), t.length());
-        int min = Math.min(s.length(), t.length());
-        for (int j2 = 0; j2 < max; j2++) {
-            if (i < min && s.charAt(i) == t.charAt(j)) {
-                i++;
-                j++;
-            } else {
-                j++;
-            }
-        }
-        if (i == min)
-            return true;
-        else
-            return false;
+		int i = 0, j = 0;
+		int max = Math.max(s.length(), t.length());
+		int min = Math.min(s.length(), t.length());
+		for (int j2 = 0; j2 < max; j2++) {
+			if (i < min && s.charAt(i) == t.charAt(j))
+				i++;
+			j++;
+		}
+		
+		return (i == min) ? true : false;
 	}
 
 }
