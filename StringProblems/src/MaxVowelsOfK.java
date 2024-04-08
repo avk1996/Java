3394414420f70e83e1ebaec117b6e0b9554a738b
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-08 09:54:55 +0530

    maximum no. of vowels in a substring of length l of string

---
+++ StringProblems/src/MaxVowelsOfK.java
@@ -0,0 +1,52 @@
+
+public class MaxVowelsOfK {
+
+	public static void main(String[] args) {
+		String s = "weallloveyou";
+		int k = 7;
+		int maxVowels = maxVowels(s, k);
+		System.out.println("max vowels: " + maxVowels);
+	}
+
+	// maximum number of vowels in a substring of length k
+	// we need to find no. of vowels within k characters of string.
+	private static int maxVowels(String s, int k) {
+		int count = 0;
+		int max = 0;
+		String vowels = "aeiouAEIOU";
+		for (int i = 0; i < s.length(); i++) {
+			if (vowels.contains(s.charAt(i) + ""))
+				count++;
+			if (i>=k && vowels.contains(s.charAt(i-k)+""))
+				count--;
+			max = Math.max(count, max);
+		}
+
+		return max;
+
+	}
+
+	// maximum number of substring within length k
+	private static int maxVowelsSubstring(String s, int k) {
+		int count = 0;
+		int max = 0;
+		String vowels = "aeiouAEIOU";
+		int i = 0;
+		int t = 0;
+		while (i < s.length()) {
+			System.out.println(1 + "." + s.charAt(i) + " " + i);
+			while (i < s.length() && vowels.contains(s.charAt(i) + "")) {
+				System.out.println(2 + "." + s.charAt(i) + " " + i);
+				count++;
+				if (count >= k)
+					return k;
+				i++;
+			}
+			max = Math.max(max, count);
+			i++;
+			count = 0;
+		}
+		return max;
+	}
+
+}
--- top_problems/src/top_problems/arrays/MaximumAverageSubarrayI.java
+++ top_problems/src/top_problems/arrays/MaximumAverageSubarrayI.java
@@ -3,7 +3,25 @@ package top_problems.arrays;
 public class MaximumAverageSubarrayI {
 
 	public static void main(String[] args) {
+		int nums[] = { 1, 12, -5, -6, 50, 4 };
+		int k = 4;
+		double maxAvg = findMaxAverage(nums, k);
+		System.out.println("Max average: " + maxAvg);
+	}
 
+	private static double findMaxAverage(int[] nums, int k) {
+		int sum = 0;
+		int avg = 0;
+		for (int i = 0; i < k; i++) {
+			sum += nums[i];
+		}
+
+		for (int i = k; i < nums.length; i++) {
+			sum += nums[i] - nums[i - k];
+			avg = Math.max(avg, sum);
+		}
+
+		return avg/1.0/k;
 	}
 
 }
