814b523ff28bb608151a8e2340f2c305b861a5f4
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-09 12:36:47 +0530

    contigious subarray of length of 1's by deleting one (0,1) using sliding window

--- top_problems/src/top_problems/arrays/CountMaxiumOfOnesAfterDeleteing.java
+++ top_problems/src/top_problems/arrays/CountMaxiumOfOnesAfterDeleteing.java
@@ -1,10 +1,8 @@
 package top_problems.arrays;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 public class CountMaxiumOfOnesAfterDeleteing {
-
 
 	public static void main(String[] args) {
 		int[] nums = { 0, 1, 1, 1, 0, 1, 1, 0, 1 };
@@ -12,13 +10,35 @@ public class CountMaxiumOfOnesAfterDeleteing {
 		System.out.println("Longest 1s after deleting 1: " + longest);
 	}
 
-	// brute force
+	// sliding window problem
 	private static int longestSubarray(int[] nums) {
+		int ans = 0;
+		int left = 0;
+		int zeros = 0;
+
+		for (int right = 0; right < nums.length; right++) {
+			if (nums[right] == 0)
+				zeros++;
+
+			while (zeros > 1) {
+				if (nums[left] == 0)
+					zeros--;
+				left++;
+			}
+
+			ans = Math.max(ans, right - left + 1);
+		}
+
+		return ans == nums.length ? ans - 1 : ans;
+	}
+
+	// brute force
+	private static int longestSubarrayBrute(int[] nums) {
 		int max = 0;
 		for (int i = 0; i < nums.length; i++) {
 			System.out.println("1. " + Arrays.toString(nums));
 			if (nums[i] == 0) {
-				
+
 				// resize
 				int temp[] = new int[nums.length - 1];
 				int k = 0;
@@ -28,27 +48,27 @@ public class CountMaxiumOfOnesAfterDeleteing {
 						k++;
 					}
 				}
-				
+
 				// count 1s
 				for (int j = 0; j < temp.length; j++) {
 					int count = 0;
-					while(j<temp.length && temp[j] != 0) {
+					while (j < temp.length && temp[j] != 0) {
 						count++;
 						j++;
 					}
 					max = Math.max(max, count);
 				}
 
-				System.out.println("2. " + Arrays.toString(temp)+ " max count: "+max);
+				System.out.println("2. " + Arrays.toString(temp) + " max count: " + max);
 			}
 		}
 		boolean flag = true;
 		for (int i = 0; i < nums.length; i++) {
-			if(nums[i]!=1)
+			if (nums[i] != 1)
 				flag = false;
 		}
-		
-		return flag ? nums.length-1 : max;
+
+		return flag ? nums.length - 1 : max;
 	}
-	
+
 }
