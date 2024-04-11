abdfeb23d4294bf03dbf5851639903f04af50cba
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-11 17:51:27 +0530

    unique occurrences in array

---
+++ top_problems/src/top_problems/arrays/UniqueOccurance.java
@@ -0,0 +1,30 @@
+package top_problems.arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class UniqueOccurance {
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 5, -2, -3, -6, -6 };
+		boolean isOccurrence = uniqueOccurrences(arr);
+		System.out.println("Contains duplicates: " + isOccurrence);
+	}
+
+	private static boolean uniqueOccurrences(int[] arr) {
+		HashMap<Integer, Integer> uniqueOcc = new HashMap<Integer, Integer>();
+		HashSet<Integer> num = new HashSet<>();
+
+		for (int i : arr) {
+			uniqueOcc.put(i, uniqueOcc.getOrDefault(i, 0) + 1);
+		}
+
+		for (Integer i : uniqueOcc.values()) {
+			num.add(i);
+		}
+
+		return uniqueOcc.size() == num.size();
+	}
+
+}
