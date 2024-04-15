b39e769a808c689ed98278be1fae1f0d2126a573
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-15 23:36:31 +0530

    using big of O n solution to product of array then itself

--- neetcode-roadmap/src/arrays_and_hashing/ProductOfArrayExceptItself.java
+++ neetcode-roadmap/src/arrays_and_hashing/ProductOfArrayExceptItself.java
@@ -11,13 +11,31 @@ public class ProductOfArrayExceptItself {
 	}
 
 	private static int[] productExceptSelf(int[] nums) {
+		int n = nums.length;
+		int[] prefix = new int[n];
+		int[] suffix = new int[n];
+		prefix[0] = 1;
+		suffix[n-1] = 1;
+		
+		for (int i = 1; i < n; i++) 
+			prefix[i] = prefix[i-1] * nums[i-1];
+		
+		for (int i = n-2; i >= 0; i--) 
+			suffix[i] = suffix[i+1] * nums[i+1];
+		
+		for (int i = 0; i < suffix.length; i++) 
+			nums[i] = prefix[i] * suffix[i];
+		
+		return nums;
+	}
+
+	private static int[] productExceptSelfBrute(int[] nums) {
 		int[] product = new int[nums.length];
 		for (int i = 0; i < nums.length; i++) {
 			int pro = 1;
-			for (int j = 0; j < nums.length; j++) {
+			for (int j = 0; j < nums.length; j++) 
 				if(i!=j)
 					pro *= nums[j];
-			}
 			product[i] = pro;
 		} 
 		return product;
